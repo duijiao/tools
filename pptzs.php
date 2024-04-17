58a8cf8371cb35261a530c325cb8706b36d4f6ef
万能盒子 <111231319+duijiao@users.noreply.github.com>
2024-04-17 17:52:58 +0800

    pptzs1.php

--- pptzs.php
+++ pptzs.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * @采集爬取学习
- <ul class="layui-clear">
- 
- <img class="lazy img" data-original="https://imgs.ymaaa.com/i/2022/12/04/638b84ed2e402.jpg" src="/images/defaultpic.gif" alt="闪豆视频下载器 v3.4.0 多平台视频批量下载"/></a>
-    <a href="/html/1346.html" class="tit" target="_blank">闪豆视频下载器 v3.4.0 多平台视频批量下载</a><a href="/html/1346.html" class="link" target="_blank">查看</a><time class="">2023-05-13</time>
- */
- header('Content-type:text/json');
+
+header('Content-type:text/json');
 index();
 function index()
 {  
@@ -45,32 +39,13 @@ function index()
     $insert=[];
     $titleRulec = "/<div class=\"descBox\">([^<>]*)<\/div>/s";
     foreach ($content[0] as $k => $v) {
-        //preg_match($titleRulec, $v, $arrc);
-        // 简介
-       // $arr['content']=trim($arrc[1]);
-        
-        
-       /* 这里判断必须用双等号 !== 才有效果,因为可能返回等同false的布尔值
-       if (strpos($str, $checkstr) !== false) {
-            $arr['image']=$imgs[1];
-        }else{
-            $arr['image']="https://www.ypppt.com".$imgs[1];
-        }*/
  
         preg_match($titleRulea, $content[0][$k], $url_title);        
         $arr['image']="https://www.ypppt.com".$url_title[1];// 标题
-        
-        
-        //preg_match($link, $linkurl[0][$k], $url);
-       
-        
+
         $strr=$url[1];
         //将字符串指定符号转为数组
         $sub_str=explode("/",$strr);
-        //$arr['link']="https://www.ypppt.com/p/d.php?aid=".$sub_str[2];// 链接
-        
-       
-        
         
         $insert[]=$arr;
     }
