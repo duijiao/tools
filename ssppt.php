e20033b4cbfd79e1aa245ed6418811e0d069115d
万能盒子 <111231319+duijiao@users.noreply.github.com>
2024-11-25 16:52:57 +0800

    Update ssppt.php

--- ssppt.php
+++ ssppt.php
@@ -1,77 +1,39 @@
 <?php
-/**
- * @采集爬取学习
- <ul class="layui-clear">
- 
- <img class="lazy img" data-original="https://imgs.ymaaa.com/i/2022/12/04/638b84ed2e402.jpg" src="/images/defaultpic.gif" alt="闪豆视频下载器 v3.4.0 多平台视频批量下载"/></a>
-    <a href="/html/1346.html" class="tit" target="_blank">闪豆视频下载器 v3.4.0 多平台视频批量下载</a><a href="/html/1346.html" class="link" target="_blank">查看</a><time class="">2023-05-13</time>
- */
- header('Content-type:text/json');
+
+header('Content-type:text/json');
 index();
 function index()
 {  
   
-    // 要爬取的网页https://www.ypppt.com/p/search.php?keyword=%E7%AE%80%E7%BA%A6&PageNo=2
     $name=$_GET["name"];
     $page=$_GET["page"];
     $url = 'https://www.ypppt.com/p/search.php?keyword='.$name.'&PageNo='.$page;
     //$url = 'https://www.ypppt.com/moban/';
     $result = httpCurl($url,[],'GET');
-    //echo $result;
-    // 图片
     $avatarRule = "/blank\"><img src=\"([^<>]*)\"><\/a>/s"; 
     preg_match_all($avatarRule,$result,$avatar);
     $preg = '/<img src=[\"|\']?(.*?)[\"|\']?\s>/i';
-   //标题
-   
     $titleRule = "/<li>.*?<\/li>/s"; 
     preg_match_all($titleRule,$result,$title);
-    //echo json_encode($title,JSON_UNESCAPED_UNICODE);
     $titleRulea = "/<span class=\left\">([^<>]*)<\/span>/s";
- 
-    // 截取内容
     $titleRuleb = "/<span class=\"left\">([^<>]*)<\/span>/s";
     preg_match_all($titleRuleb,$result,$content);
-    
-   // echo json_encode($content,JSON_UNESCAPED_UNICODE);
     $link = "/<a href=\"\/.*?\/([^<>]*).html\" class=\"p-title\" target=\"_blank\">/s";
     preg_match_all($link,$result,$linkurl);
-    
-    
     $insert=[];
     $titleRulec = "/<div class=\"descBox\">([^<>]*)<\/div>/s";
     foreach ($avatar[0] as $k => $v) {
-        //preg_match($titleRulec, $v, $arrc);
-        // 简介
-       // $arr['content']=trim($arrc[1]);
- 
-        // 图片
         preg_match($avatarRule, $avatar[0][$k], $imgs);
         $arr['image']="https://www.ypppt.com".$imgs[1];
         $str = $imgs[1];
         $checkstr = 'http';
-        
-       /* 这里判断必须用双等号 !== 才有效果,因为可能返回等同false的布尔值
-       if (strpos($str, $checkstr) !== false) {
-            $arr['image']=$imgs[1];
-        }else{
-            $arr['image']="https://www.ypppt.com".$imgs[1];
-        }*/
- 
-        // preg_match($titleRulea, $content[0][$k], $url_title);        
-        // $arr['title']=$url_title[1];// 标题
-        
-        
         preg_match($link, $linkurl[0][$k], $url); 
         $arr['link1']="https://www.ypppt.com/".$url[1].".html";
         $strr=$url[1];
         //将字符串指定符号转为数组
         $sub_str=explode("/",$strr);
         $arr['link']=$sub_str[2];// 链接
-        
-        
-        
-        
+
         $insert[]=$arr;
     }
     //var_dump($insert);
